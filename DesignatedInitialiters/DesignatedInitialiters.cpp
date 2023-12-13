0956e73cf2f2595a344af82f701bd1308b8064fa
MIGUEL PIE BARGALLO <90567646+piebargallo@users.noreply.github.com>
2023-12-13 09:01:57 +0100

    Add files via upload

---
+++ DesignatedInitialiters/DesignatedInitialiters.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <format>
+using namespace std;
+
+struct Employee {
+	char firstInitial;
+	char lastInitial;
+	int  employeeNumber;
+	int  salary{ 75'000 };
+};
+
+int main()
+{
+	// Initialize all data members
+	Employee anEmployee1{
+		.firstInitial = 'J',
+		.lastInitial = 'D',
+		.employeeNumber = 42,
+		.salary = 80'000
+	};
+
+	// Initialize all data members, except employeeNumber
+	Employee anEmployee2{
+		.firstInitial = 'J',
+		.lastInitial = 'D',
+		.salary = 80'000
+	};
+
+	// Initialize all data members, except employeeNumber, and salary
+	Employee anEmployee3{
+		.firstInitial = 'J',
+		.lastInitial = 'D'
+	};
+
+	cout << format("anEmploye3 {} {}", anEmployee3.firstInitial, anEmployee3.lastInitial) << endl;
+	return 0;
+}
