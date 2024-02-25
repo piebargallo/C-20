cd67ead8ba1bd2b184f7552e155f609d3183fa3d
MIGUEL PIE BARGALLO <90567646+piebargallo@users.noreply.github.com>
2024-02-25 11:53:28 +0100

    Add files via upload

---
+++ 12_ConditionalOperator/main.cpp
@@ -0,0 +1,20 @@
+// Author by: Miquel Pié
+
+#include <iostream>
+using std::cout;
+using std::endl;
+
+int main()
+{
+	int i{ 4 };
+	
+	cout << (i > 2 ? "yes" : "no") << endl;
+	/*
+	if (i > 2) {
+		cout << "yes" << endl;
+	} else {
+		cout << "no" << endl;
+	}
+	*/
+	return 0;
+}
